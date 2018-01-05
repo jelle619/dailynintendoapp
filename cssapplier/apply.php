dea2fcf0523c7d954f0359cdb5eca7bbef418496
Jelle Z <jellezwiers@icloud.com>
2018-01-05 13:53:49 +0100

    Update apply.php

--- cssapplier/apply.php
+++ cssapplier/apply.php
@@ -1,20 +1,7 @@
 <html>
 <?php
-$url = 'http://twitter.com/statuses/user_timeline.rss?screen_name=google&count=6';
-$homepage = curl($url);
-echo $homepage; //information recieved
+$homepage = file_get_contents('https://www.dailynintendo.nl');
+echo $homepage;
 ?>
-<link rel='stylesheet' href='dailynintendoapp/style.css' type='text/css'/>
-
-
+<link rel='stylesheet' href='my_changes.css' type='text/css'/>
 </html>
-
-function curl($url) {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-    $data = curl_exec($ch);
-    curl_close($ch);
-
-    return $data;
-}
