28d731e0a4a8e0215c1773a9937d8b448c548eb2
Jelle Z <jellezwiers@icloud.com>
2018-01-05 13:58:49 +0100

    Update apply.php

--- cssapplier/apply.php
+++ cssapplier/apply.php
@@ -3,5 +3,5 @@
 $homepage = file_get_contents('https://www.dailynintendo.nl');
 echo $homepage;
 ?>
-<link rel='stylesheet' href='my_changes.css' type='text/css'/>
+<link rel='stylesheet' href='style.css' type='text/css'/>
 </html>
